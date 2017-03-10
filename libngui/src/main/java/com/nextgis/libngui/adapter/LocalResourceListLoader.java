bf2d985578e9a1c8ca6629fccc53fd90d4e88a78
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-03-10 19:23:41 +0300

    Delete fs restriction in LocalResourceSelectDialog

--- libngui/src/main/java/com/nextgis/libngui/adapter/LocalResourceListLoader.java
+++ libngui/src/main/java/com/nextgis/libngui/adapter/LocalResourceListLoader.java
@@ -5,7 +5,7 @@
  * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2012-2016 NextGIS, info@nextgis.com
+ * Copyright (c) 2012-2017 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
@@ -105,11 +105,9 @@ public class LocalResourceListLoader
         File parentFile = mPath.getParentFile();
         if (null != parentFile) {
             // TODO: get list of sdcards
-            if (!parentFile.getAbsolutePath().equals("/storage/emulated")) {
-                LocalResourceListItem item =
-                        new LocalResourceListItem(parentFile, FILETYPE_PARENT, false, false);
-                resources.add(item);
-            }
+            LocalResourceListItem item =
+                    new LocalResourceListItem(parentFile, FILETYPE_PARENT, false, false);
+            resources.add(item);
         }
 
         File[] files = mPath.listFiles();
